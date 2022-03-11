cc2bcea6862771988385a2623382daacca38e9f3
CakesTwix <oleg.kiryazov@gmail.com>
2022-03-11 15:43:31 +0200

    nhentai: simple refactor

--- nhentai.py
+++ nhentai.py
@@ -8,7 +8,7 @@
 
 """
 
-__version__ = (1, 0, 0)
+__version__ = (1, 0, 1)
 
 # requires: requests hentai
 # meta pic: https://seeklogo.com/images/H/hentai-haven-logo-B9D8C4B3B8-seeklogo.com.png
@@ -23,6 +23,38 @@ from .. import loader, utils
 
 logger = logging.getLogger(__name__)
 
+# Utils
+def StringBuilder(Hentai):
+    id_nh = Hentai.id
+    eng_name = Hentai.title()
+    link = Hentai.url
+    total_pages = Hentai.num_pages
+    total_favorites = Hentai.num_favorites
+    tags = ""
+    for tag in Hentai.tag:
+        tags += f"{tag.name} "
+
+    text = f"<a href={link}>{eng_name}</a> [{id_nh}]\n\n"
+    text += f"{tags} \n"
+    text += f"❤️ {total_favorites} | 📄 {total_pages}"
+    return text
+
+
+def ListHentaiBuilder(Hentais):
+    text = ""
+    i = 1
+    for Hentai in Hentais:
+        id_nh = Hentai.id
+        eng_name = Hentai.title()
+        link = Hentai.url
+        total_pages = Hentai.num_pages
+        total_favorites = Hentai.num_favorites
+
+        text += f"{i}: <a href={link}>{eng_name}</a> [{id_nh}] / "
+        text += f"❤️ {total_favorites} | 📄 {total_pages} \n"
+        i += 1
+    return text
+
 
 @loader.unrestricted
 @loader.ratelimit
@@ -34,43 +66,13 @@ class NHentaiMod(loader.Module):
         "name": "NHentai",
     }
 
-    def StringBuilder(self, Hentai):
-        id_nh = Hentai.id
-        eng_name = Hentai.title()
-        link = Hentai.url
-        total_pages = Hentai.num_pages
-        total_favorites = Hentai.num_favorites
-        tags = ""
-        for tag in Hentai.tag:
-            tags += f"{tag.name} "
-
-        text = f"<a href={link}>{eng_name}</a> [{id_nh}]\n\n"
-        text += f"{tags} \n"
-        text += f"❤️ {total_favorites} | 📄 {total_pages}"
-        return text
-
-    def ListHentaiBuilder(self, Hentais):
-        text = ""
-        i = 1
-        for Hentai in Hentais:
-            id_nh = Hentai.id
-            eng_name = Hentai.title()
-            link = Hentai.url
-            total_pages = Hentai.num_pages
-            total_favorites = Hentai.num_favorites
-
-            text += f"{i}: <a href={link}>{eng_name}</a> [{id_nh}] / "
-            text += f"❤️ {total_favorites} | 📄 {total_pages} \n"
-            i += 1
-        return text
-
     @loader.unrestricted
     @loader.ratelimit
     async def nhrandomcmd(self, message):
         """Random hentai manga"""
         await message.delete()
         hentai_info = Utils.get_random_hentai()
-        text = self.StringBuilder(hentai_info)
+        text = StringBuilder(hentai_info)
 
         await message.client.send_file(message.chat_id, hentai_info.cover, caption=text)
 
@@ -81,7 +83,7 @@ class NHentaiMod(loader.Module):
         args = utils.get_args(message)
         if args:
             hentai_info = Utils.search_by_query(args)
-            text = self.ListHentaiBuilder(hentai_info)
+            text = ListHentaiBuilder(hentai_info)
 
             await utils.answer(message, text)
         else:
@@ -97,7 +99,7 @@ class NHentaiMod(loader.Module):
         if args[0].isdigit():
             try:
                 hentai_info = Hentai(args[0])
-                text = self.StringBuilder(hentai_info)
+                text = StringBuilder(hentai_info)
                 await message.client.send_file(
                     message.chat_id, hentai_info.cover, caption=text
                 )
