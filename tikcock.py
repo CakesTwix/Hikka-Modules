6babaed401a7c585daee8ad597056c919268cfdb
CakesTwix <oleg.kiryazov@gmail.com>
2022-04-01 19:54:25 +0300

    tiktok: init
    Just updated someone else's module, as well as adding a download with a watermark. The author, unfortunately, could not find so I will leave a link to the source, where I took the module.

---
+++ tikcock.py
@@ -0,0 +1,61 @@
+"""
+
+    █▀▀ ▄▀█ █▄▀ █▀▀ █▀ ▀█▀ █░█░█ █ ▀▄▀
+    █▄▄ █▀█ █░█ ██▄ ▄█ ░█░ ▀▄▀▄▀ █ █░█
+
+    Copyleft 2022 t.me/cakestwix
+    This program is free software; you can redistribute it and/or modify
+
+    Source - https://github.com/sm1ke000FriendlyTelegram/Friendly-Telegram/raw/main/TikTokMOD.py
+
+"""
+
+__version__ = (1, 0, 0)
+
+
+import logging
+import asyncio
+from .. import loader, utils
+from telethon import types
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class TikTokMod(loader.Module):
+    """Download tt video via @tikdobot"""
+
+    strings = {
+        "name": "TikTokDownloader",
+        "no_args": "Not found args, pls check help",
+    }
+
+    @loader.unrestricted
+    @loader.ratelimit
+    async def dttnwcmd(self, message):
+        """Get tt video without watermark"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await utils.answer(message, self.strings["no_args"])
+
+        r = await message.client.inline_query('tikdobot', args)
+        await message.client.send_file(message.to_id, r[1].result.content.url)
+        await message.delete()
+
+    
+    @loader.unrestricted
+    @loader.ratelimit
+    async def dttcmd(self, message):
+        """Get tt video with watermark"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await utils.answer(message, self.strings["no_args"])
+
+        r = await message.client.inline_query('tikdobot', args)
+        await message.client.send_file(message.to_id, r[0].result.content.url)
+        await message.delete()
+
+
+
+
+
