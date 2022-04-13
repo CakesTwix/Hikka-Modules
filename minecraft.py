67914f279e4eb709db8257346aa53bbfb7d9a8d2
CakesTwix <oleg.kiryazov@gmail.com>
2022-04-13 22:41:19 +0300

    minecraft: add ping command (Server stuff)

--- minecraft.py
+++ minecraft.py
@@ -8,7 +8,7 @@
 
 """
 
-__version__ = (1, 0, 0)
+__version__ = (1, 1, 0)
 
 # requires: aiohttp
 # meta pic: https://icons.iconarchive.com/icons/blackvariant/button-ui-requests-2/1024/Minecraft-2-icon.png
@@ -25,11 +25,16 @@ class InlineMinecraftInfoMod(loader.Module):
     """Information about players and server status"""
 
     strings = {
-        "name": "InlineMinecraftInfo",
+        "name": "MinecraftInfo",
         "error_message": "🚫 This entity does not exist or you entered it incorrectly",
         "about_user": "<b>Available player information</b> <code>{}</code>:'\n",
+        "about_server": "<b>Available server information</b> <code>{}</code>:'\n",
         "username": "<b>Username:</b> <code>{}</code>\n",
-        "id": "<b>Id:</b> <code>{}</code>\n"
+        "id": "<b>Id:</b> <code>{}</code>\n",
+        "description": "<b>Description</b>: {}\n",
+        "latency": "<b>Latency</b>: {}\n",
+        "players": "<b>Players</b>: {} in {}\n",
+        "versions": "<b>Versions</b>: {}\n",
     }
 
     base_url = "https://api.minetools.eu"
@@ -53,6 +58,25 @@ class InlineMinecraftInfoMod(loader.Module):
 
             for texture in user['decoded']['textures']:
                 text += f"<b>{texture}</b>: <a href={user['decoded']['textures'][texture]['url']}>URL</a>\n"
+
+            await utils.answer(message, text)
+
+    
+    async def mpingcmd(self, message):
+        """Ping minecraft server"""
+        args = utils.get_args_raw(message)
+        if args:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"{self.base_url}/ping/{args}") as get:
+                    data = await get.json()
+                    if "error" in data:
+                        return await utils.answer(message, self.strings["error_message"])
+            
+            text = self.strings["about_user"].format(args)
+            text += self.strings["description"].format(data["description"])
+            text += self.strings["latency"].format(data["latency"])
+            text += self.strings["players"].format(data["players"]["online"], data["players"]["max"])
+            text += self.strings["versions"].format(data["version"]["name"])
 
             await utils.answer(message, text)
                         
