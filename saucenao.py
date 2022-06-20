915b470941baa64c52e575abb436da93b0b08f4c
CakesTwix <oleg.kiryazov@gmail.com>
2022-06-20 09:52:34 +0300

    saucenao: init

---
+++ saucenao.py
@@ -0,0 +1,86 @@
+"""
+
+    █▀▀ ▄▀█ █▄▀ █▀▀ █▀ ▀█▀ █░█░█ █ ▀▄▀
+    █▄▄ █▀█ █░█ ██▄ ▄█ ░█░ ▀▄▀▄▀ █ █░█
+
+    Copyleft 2022 t.me/CakesTwix
+    This program is free software; you can redistribute it and/or modify
+
+"""
+
+__version__ = (1, 0, 0)
+
+# meta pic: https://allvpn.ru/assets/upload/t-200x200-7439447981535195421.png
+# meta developer: @cakestwix_mods
+# requires: saucenao_api
+# scope: inline
+# scope: hikka_only
+# scope: hikka_min 1.2.7
+
+import logging
+
+from .. import loader, utils
+from saucenao_api import AIOSauceNao
+from saucenao_api.errors import ShortLimitReachedError, LongLimitReachedError
+from saucenao_api.containers import BasicSauce
+
+logger = logging.getLogger(__name__)
+
+
+def string_builder(sauce_item):
+    string = "Unsupported type, sorry("
+    if isinstance(sauce_item, BasicSauce):
+        string = f"<b>Similarity</b>: <code>{sauce_item.similarity}</code>\n\n"
+
+        string += f"<b>Title</b>: <code>{sauce_item.title}</code>\n"
+        string += f"<b>Author</b>: <code>{sauce_item.author}</code>\n"
+        string += f"<b>Urls</b>: {' '.join(sauce_item.urls)}\n"
+
+    return string
+
+
+@loader.tds
+class SauceNaoMod(loader.Module):
+    """🔎 SauceNao - image source locator"""
+
+    strings = {
+        "name": "🔎 SauceNao",
+        "cfg_api_key": "https://saucenao.com/user.php?page=search-api",
+    }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            "CONFIG_API_KEY",
+            None,
+            lambda: self.strings("cfg_api_key"),
+        )
+
+    # Just commands
+
+    async def sauceurlcmd(self, message):
+        """🔗 Search for the source by link"""
+        if not self.config["CONFIG_API_KEY"]:
+            return await utils.answer(message, "🚫 <b>No API Key</b>")
+
+        # Get args from message
+        if url := utils.get_args_raw(message):
+            if utils.check_url(utils.get_args_raw(message)):
+                async with AIOSauceNao(self.config["CONFIG_API_KEY"]) as aio:
+                    try:
+                        results = await aio.from_url(url)
+                    except ShortLimitReachedError:
+                        return await utils.answer(message, "🚫 <b>ShortLimitReachedError</b>")
+                    except LongLimitReachedError:
+                        return await utils.answer(message, "🚫 <b>LongLimitReachedError</b>")
+
+                    await self.inline.gallery(
+                        message,
+                        [url_photo.thumbnail for url_photo in results],
+                        [
+                            f"<b>Request limits (per 30 seconds limit)</b>: <code>{results.short_remaining}</code>\n<b>Request limits (per day limit)</b>: <code>{results.long_remaining}</code>\n"
+                            + string_builder(item)
+                            for item in results
+                        ],
+                    )
+            else:
+                await utils.answer(message, "🚫 <b>Wrong Url</b>")
