25320ffc2ccbcb59ee6aa28a375009c27624bf90
CakesTwix <oleg.kiryazov@gmail.com>
2022-06-26 17:21:24 +0300

    fdroid: Parse IzzySoft site

--- fdroid.py
+++ fdroid.py
@@ -8,7 +8,7 @@
 
 """
 
-__version__ = (1, 0, 0)
+__version__ = (1, 1, 0)
 
 # meta pic: https://forum.f-droid.org/uploads/default/original/2X/c/cfb2c14973c28415b0e5b5f7adef9c8288cd8609.png
 # meta developer: @cakestwix_mods
@@ -32,93 +32,57 @@ from ..inline.types import InlineQuery
 
 logger = logging.getLogger(__name__)
 
-url = "https://search.f-droid.org/?lang=en&page="
 
+async def fdroid_search(search_app = "") -> dict:
+    fdroid_main = "https://apt.izzysoft.de/fdroid/index.php?repo=main" # FDroid
 
-async def fdroid_search(search_app) -> dict:
-    """From https://github.com/imthepooh/fdroidsrch/blob/master/main.py"""
-    fin_dict = []
-
-    i = 1
-    url_set = url + str(i) + "&q=" + search_app
     async with httpx.AsyncClient() as client:
-        s_page = await client.get(url_set)
-    # fetch the search results
-    soup = BeautifulSoup(s_page.content, "html.parser")
-    num = 0
+        html = (await client.post(fdroid_main, data={"limit": 50, "searchterm": search_app, "doFilter": "Go%21"})).content
 
-    # check if the search results are multipaged
-    for nums in soup.find_all("span", class_="step-links"):
-        try:
-            num = int(nums.get_text())
-        except Exception:
-            num_str = nums.get_text()
+    soup = BeautifulSoup(html, "html.parser")
+    apps = []
 
-    # set the number of pages to be scrapped
-    i = 1 if num == 0 else num
-    for n in range(1, i + 1):
-        if n > 1:
-            url_set = url + str(n) + "&q=" + search_app
-            async with httpx.AsyncClient() as client:
-                s_page = await client.get(url_set)
-                soup = BeautifulSoup(s_page.content, "html.parser")
+    html_apps = soup.find_all("div", class_="approw")
 
-        # get app name, details and url
-        for app_h in soup.find_all("a", class_="package-header"):
-            app_n = app_h.find("h4", class_="package-name")
-            app_s = app_h.find("span", class_="package-summary")
-            app_l = app_h.find("span", class_="package-license")
-            app_i = app_h.find("img", class_="package-icon")
+    for app in html_apps:
+        buff = {"Name": app.find("span", class_="boldname").get_text()}
+        buff["Desc"] = app.find_all("div", class_="appdetailcell")[-2].get_text()
+        buff["Icon"] = app.find("img")["src"] if app.find("img")["src"] != "/shared/images/spacer.gif" else "https://f-droid.org/repo/com.termux.tasker/en-US/icon.png"
 
-            # prepare result string
-            fin_dict.append(
-                {
-                    "Name": app_n.get_text().strip(),
-                    "Desc": app_s.get_text().strip(),
-                    "Url": app_h["href"].strip(),
-                    "License": app_l.get_text().strip(),
-                    "Icon": app_i["src"],
-                }
-            )
-        
-        if len(fin_dict) >= 40:
-            return fin_dict
+        buff["Minor-Details"] = app.find_all("span", class_="minor-details")
+        buff["Links"] = app.find_all("a", class_="paddedlink")[1:]
+        apps.append(buff)
+
+    return apps
     
-    return fin_dict
-    
-
+def StringBuilder(app):
+    return f"<code>{app['Name']}</code> {app['Minor-Details'][0].get_text()} ({app['Minor-Details'][1].get_text()})\n\n{app['Desc']}"
 
 class FDroidMod(loader.Module):
     """Search for android apps from FDroid"""
 
     strings = {
         "name": "FDroid",
-        "no_args": "🚫 Not found args, pls check help",
         "no_apps": "🚫 Unfortunately, I couldn't find any applications",
-        "link": "🔗 Link",
     }
 
     strings_ru = {
         "name": "FDroid",
-        "no_args": "🚫 Аргументы не найдены, пожалуйста, проверьте справку",
         "no_apps": "🚫 К сожалению, не нашел приложений",
-        "link": "🔗 Ссылка",
     }
 
     async def fdroidcmd(self, message):
         """Find the app in the FDroid catalog"""
         args = utils.get_args_raw(message)
-        if not args:
-            return await utils.answer(message, self.strings["no_args"])
-
         if apps := await fdroid_search(args):
-            await self.inline.gallery(
-                message,
-                [app["Icon"] for app in apps],
-                [
-                    f"<code>{app['Name']}</code> ({app['License']})\n\n{app['Desc']}\n{hlink(self.strings['link'], app['Url'])}"
-                    for app in apps
-                ],
+            markup = [[{"text": app.get_text(), "url": app["href"]} for app in apps[0]["Links"]]]
+            if len(apps) != 1:
+                markup.append([{"text":"➡️","callback": self.fdroid_pagination__callback, "args": (apps, 0, "+")}])
+            await self.inline.form(
+                text=StringBuilder(apps[0]),
+                message=message,
+                # photo=apps[0]["Icon"],
+                reply_markup=markup,
             )
         else:
             await utils.answer(message, self.strings["no_apps"])
@@ -127,32 +91,48 @@ class FDroidMod(loader.Module):
     async def fdroid_inline_handler(self, query: InlineQuery) -> None:
         """Find the app in the FDroid catalog (Inline)"""
         query_args = query.args
-        if not query_args:
-            return await query.e400()
 
         if apps := await fdroid_search(query_args):
             InlineQueryResult = []
             for app in apps:
                 # Generate button
                 markup = InlineKeyboardMarkup()
-                markup.insert(InlineKeyboardButton(self.strings["link"], app["Url"]))
+                for link in app["Links"]:
+                    markup.insert(InlineKeyboardButton(link.get_text(), link["href"]))
 
                 # Add InlineQueryResultArticle
                 InlineQueryResult.append(
                     InlineQueryResultArticle(
                         id=utils.rand(64),
-                        title=f'{app["Name"]} ({app["License"]})',
+                        title=f'{app["Name"]} ({app["Minor-Details"][1].get_text()})',
                         description=app["Desc"],
                         input_message_content=InputTextMessageContent(
-                            f"<code>{app['Name']}</code> ({app['License']})\n\n{app['Desc']}",
+                            StringBuilder(app),
                             "HTML",
                             disable_web_page_preview=True,
                         ),
                         reply_markup=markup,
-                        thumb_url=app["Icon"],
+                        # thumb_url=app["Icon"],
                     )
                 )
 
             await query.answer(InlineQueryResult, cache_time=0)
         else:
             await query.e404()
+    
+    # Just callbacks
+
+    async def fdroid_pagination__callback(self, call, apps, index, type_button):
+        markup = [[{"text": app.get_text(), "url": app["href"]} for app in apps[index]["Links"]],[]]
+        if type_button == "+":
+            index += 1
+            markup[1].append({"text":"⬅️","callback": self.fdroid_pagination__callback, "args": (apps, index, "-")})
+            if index != len(apps) - 1:
+                markup[1].append({"text":"➡️","callback": self.fdroid_pagination__callback, "args": (apps, index, "+")})
+        else:
+            index -= 1
+            if index != 0:
+                markup[1].append({"text":"⬅️","callback": self.fdroid_pagination__callback, "args": (apps, index, "-")})
+
+            markup[1].append({"text":"➡️","callback": self.fdroid_pagination__callback, "args": (apps, index, "+")})
+        await call.edit(text=StringBuilder(apps[index]), reply_markup=markup)
